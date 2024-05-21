6a10ccf974172cba9a7a52c2a1d0adedec43cbec
Jörg Schönberger <office@webchills.at>
2024-05-21 16:27:29 +0200

    Version 1.3.0

--- NEUE DATEIEN/DEINADMIN/includes/installers/ga4-gads-cookie-consent/1_3_0.php
+++ NEUE DATEIEN/DEINADMIN/includes/installers/ga4-gads-cookie-consent/1_3_0.php
@@ -5,7 +5,7 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: 1_2_0.php 2024-05-21 16:24:51Z webchills $
+ * @version $Id: 1_3_0.php 2024-05-21 16:24:51Z webchills $
  */
 
-$db->Execute("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '1.2.0' WHERE configuration_key = 'GA4_MODUL_VERSION';");
+$db->Execute("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '1.3.0' WHERE configuration_key = 'GA4_MODUL_VERSION';");
